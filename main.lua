356e08c1c9bc272622a2b6d312ea31de5dc71164
Andrew DiMola <andrewdimola@gmail.com>
2018-03-13 02:10:19 -0400

    New class section + returning
    - added the ability to return to the main screen from the "fight" and "class" screens
    - added the four base classes to the "class" screen
    - general refactoring, with improved comments

--- main.lua
+++ main.lua
@@ -1,9 +1,6 @@
 function love.load()
-	
-	-- GENERAL SETTINGS
-	gameWidth = love.graphics.getWidth()
-	gameHeight = love.graphics.getHeight()
-	
+
+	loadGeneralSettings()
 	loadSelectScreen()
 	loadFightScreen()
 	loadClassScreen()
@@ -40,53 +37,135 @@ end
 ----- love.load utility functions -----
 ---------------------------------------
 
+function loadGeneralSettings()
+	
+	-- GENERAL SETTINGS
+	gameWidth = love.graphics.getWidth()
+	gameHeight = love.graphics.getHeight()
+	OptionsRGB = {255,255,255}
+	OptionsInFocusRGB = {0,0,0}
+	
+	-- RETURN OPTION SETTINGS
+	returnOption = "RETURN"
+	returnOptionFont = love.graphics.newFont(36)
+	returnOptionWidth = returnOptionFont:getWidth(returnOption)
+	returnOptionHeight = returnOptionFont:getHeight(returnOption)
+	returnOptionX = (gameWidth / 2) - (returnOptionWidth / 2)
+	returnOptionY = gameHeight - 100	
+	isReturnOptionActive = false
+
+end
+
 function loadSelectScreen()
 
-	isSelectScreen = true
-	isPlayedSoundEffect = false
+	-- GENERAL SETTINGS
 	selectScreenRGB = {131,192,240}
-	selectScreenOptionsRGB = {255,255,255}
-	selectScreenOptionsInFocusRGB = {0,0,0}
-	selectScreenTitleFont = love.graphics.newFont(48)	
-	selectScreenOptionFont = love.graphics.newFont(36)
-	selectScreenMusic = love.audio.newSource("soundtrack/select_screen.mp3", "stream")
-	mouseClickMusic = love.audio.newSource("soundtrack/mouse_click.mp3", "static")
-	mouseClickMusic:setLooping(false)
+	isSelectScreen = true
 
 	-- TITLE SETTINGS
 	gameTitle = "Base Class+"
+	selectScreenTitleFont = love.graphics.newFont(48)	
 	gameTitleWidth = selectScreenTitleFont:getWidth(gameTitle)
 	gameTitleHeight = selectScreenTitleFont:getHeight(gameTitle)
 	gameTitleX = (gameWidth / 2) - (gameTitleWidth / 2)
-	gameTitleY = 50
+	gameTitleY = 25
 	
-	-- OPTION SETTINGS
+	-- FIGHT OPTION SETTINGS
 	fightOption = "FIGHT!"
-	isFightOptionActive = false
-	fightOptionWidth = selectScreenOptionFont:getWidth(fightOption)
-	fightOptionHeight = selectScreenOptionFont:getHeight(fightOption)
+	fightOptionFont = love.graphics.newFont(36)
+	fightOptionWidth = fightOptionFont:getWidth(fightOption)
+	fightOptionHeight = fightOptionFont:getHeight(fightOption)
 	fightOptionX = (gameWidth / 2) - (fightOptionWidth / 2)
 	fightOptionY = gameHeight / 2
+	isFightOptionActive = false
 	
-	isClassOptionActive = false
+	-- CLASS OPTION SETTINGS
 	classOption = "LEARN THE CLASSES!"
-	classOptionWidth = selectScreenOptionFont:getWidth(classOption)
-	classOptionHeight = selectScreenOptionFont:getHeight(classOption)
-	classOptionX = (gameWidth / 2)  - (classOptionWidth / 2)
+	classOptionFont = love.graphics.newFont(36)
+	classOptionWidth = classOptionFont:getWidth(classOption)
+	classOptionHeight = classOptionFont:getHeight(classOption)
+	classOptionX = (gameWidth / 2) - (classOptionWidth / 2)
 	classOptionY = (gameHeight / 2) + 50
+	isClassOptionActive = false
+	
+	-- MUSIC SETTINGS
+	selectScreenMusic = love.audio.newSource("soundtrack/select_screen.mp3", "stream")
+	mouseClickMusic = love.audio.newSource("soundtrack/mouse_click.mp3", "static")
+	mouseClickMusic:setLooping(false)
+	isPlayedMouseClick = false
 end
 
 function loadFightScreen()
 	
-	isFightScreen = false
+	-- GENERAL SETTINGS
 	fightScreenRGB = {255,127,39}
+	isFightScreen = false
+	
+	-- MUSIC SETTINGS
 	fightScreenMusic = love.audio.newSource("soundtrack/fight_screen.mp3", "stream")
 end
 
 function loadClassScreen()
 	
-	isClassScreen = false
+	-- GENERAL SETTINGS
 	classScreenRGB = {163,73,164}
+	isClassScreen = false
+	
+	-- TITLE SETTINGS
+	classScreenTitle = "The Base Classes"
+	classScreenTitleFont = love.graphics.newFont(48)	
+	classScreenTitleWidth = classScreenTitleFont:getWidth(classScreenTitle)
+	classScreenTitleHeight = classScreenTitleFont:getHeight(classScreenTitle)
+	classScreenTitleX = (gameWidth / 2) - (classScreenTitleWidth / 2)
+	classScreenTitleY = 25
+	
+	-- IMAGE SETTINGS
+	brute = love.graphics.newImage("images/brute.png") -- Used as reference for base dimensions for drawing other classes
+	speedster = love.graphics.newImage("images/speedster.png")
+	acolyte = love.graphics.newImage("images/acolyte.png")
+	zealot = love.graphics.newImage("images/zealot.png")
+	classDescriptionWidth = brute:getWidth()
+	classDescriptionHeight = brute:getHeight()
+	classDescriptionX = (gameWidth / 2)  - (classDescriptionWidth / 2)
+	classDescriptionY = gameHeight / 6
+	
+	-- BRUTE OPTION SETTINGS
+	bruteName = "Brute"
+	bruteNameFont = love.graphics.newFont(24)	
+	bruteNameWidth = bruteNameFont:getWidth(bruteName)
+	bruteNameHeight = bruteNameFont:getHeight(bruteName)
+	bruteNameX = 50
+	bruteNameY = gameHeight - 50
+	isBruteNameActive = false
+	
+	-- SPEEDSTER OPTION SETTINGS
+	speedsterName = "Speedster"
+	speedsterNameFont = love.graphics.newFont(24)	
+	speedsterNameWidth = speedsterNameFont:getWidth(speedsterName)
+	speedsterNameHeight = speedsterNameFont:getHeight(speedsterName)
+	speedsterNameX = 250
+	speedsterNameY = gameHeight - 50
+	isSpeedsterNameActive = false
+	
+	-- ACOLYTE OPTION SETTINGS
+	acolyteName = "Acolyte"
+	acolyteNameFont = love.graphics.newFont(24)	
+	acolyteNameWidth = acolyteNameFont:getWidth(acolyteName)
+	acolyteNameHeight = acolyteNameFont:getHeight(acolyteName)
+	acolyteNameX = 450
+	acolyteNameY = gameHeight - 50
+	isAcolyteNameActive = false
+
+	-- ZEALOT OPTION SETTINGS
+	zealotName = "Zealot"
+	zealotNameFont = love.graphics.newFont(24)	
+	zealotNameWidth = zealotNameFont:getWidth(zealotName)
+	zealotNameHeight = zealotNameFont:getHeight(zealotName)
+	zealotNameX = 650
+	zealotNameY = gameHeight - 50
+	isZealotNameActive = false
+	
+	-- MUSIC SETTINGS
 	classScreenMusic = love.audio.newSource("soundtrack/class_screen.mp3", "stream")
 end
 
@@ -96,15 +175,20 @@ end
 
 function updateSelectScreen()
 
+	-- MUSIC UPDATES
+	fightScreenMusic:stop()
+	classScreenMusic:stop()
 	selectScreenMusic:play() 
 		
+	-- FIGHT OPTION UPDATES
 	if mouseX >= fightOptionX and mouseX <= (fightOptionX + fightOptionWidth) and mouseY >= fightOptionY and mouseY <= (fightOptionY + fightOptionHeight) then
-		if not isPlayedSoundEffect then
+		if not isPlayedMouseClick then
 			mouseClickMusic:play()
-			isPlayedSoundEffect = true
+			isPlayedMouseClick = true
 		end
 		
 		if mouseClicked then 
+			isPlayedMouseClick = false
 			isSelectScreen = false 
 			isFightScreen = true
 		end
@@ -114,13 +198,15 @@ function updateSelectScreen()
 		isFightOptionActive = false
 	end
 
+	-- CLASS OPTION UPDATES
 	if mouseX >= classOptionX and mouseX <= (classOptionX + classOptionWidth) and mouseY >= classOptionY and mouseY <= (classOptionY + classOptionHeight) then 
-		if not isPlayedSoundEffect then
+		if not isPlayedMouseClick then
 			mouseClickMusic:play()
-			isPlayedSoundEffect = true
+			isPlayedMouseClick = true
 		end
 		
 		if mouseClicked then 
+			isPlayedMouseClick = false
 			isSelectScreen = false 
 			isClassScreen = true
 		end
@@ -130,19 +216,128 @@ function updateSelectScreen()
 		isClassOptionActive = false
 	end
 	
-	if not isFightOptionActive and not isClassOptionActive then isPlayedSoundEffect = false end
+	if not isFightOptionActive and not isClassOptionActive then isPlayedMouseClick = false end
 end
 
 function updateFightScreen()
 	
+	-- MUSIC UPDATES
 	selectScreenMusic:stop()
 	fightScreenMusic:play() 
+	
+	-- RETURN OPTION UPDATES
+	if mouseX >= returnOptionX and mouseX <= (returnOptionX + returnOptionWidth) and mouseY >= returnOptionY and mouseY <= (returnOptionY + returnOptionHeight) then 
+		if not isPlayedMouseClick then
+			mouseClickMusic:play()
+			isPlayedMouseClick = true
+		end
+		
+		if mouseClicked then 
+			isFightScreen = false 
+			isSelectScreen = true
+		end
+		
+		isReturnOptionActive = true
+	else 
+		isReturnOptionActive = false
+	end
+	
+	if not isReturnOptionActive then isPlayedMouseClick = false end
 end
 
 function updateClassScreen()
 	
+	-- MUSIC UPDATES
 	selectScreenMusic:stop()
 	classScreenMusic:play() 
+	
+	-- RETURN OPTION UPDATES
+	if mouseX >= returnOptionX and mouseX <= (returnOptionX + returnOptionWidth) and mouseY >= returnOptionY and mouseY <= (returnOptionY + returnOptionHeight) then 
+		if not isPlayedMouseClick then
+			mouseClickMusic:play()
+			isPlayedMouseClick = true
+		end
+		
+		if mouseClicked then 
+			isClassScreen = false 
+			isSelectScreen = true
+		end
+		
+		isReturnOptionActive = true
+	else 
+		isReturnOptionActive = false
+	end
+	
+	-- BRUTE OPTION UPDATES
+	if mouseX >= bruteNameX and mouseX <= (bruteNameX + bruteNameWidth) and mouseY >= bruteNameY and mouseY <= (bruteNameY + bruteNameHeight) then 
+		if not isPlayedMouseClick then
+			mouseClickMusic:play()
+			isPlayedMouseClick = true
+		end
+		
+		if mouseClicked then 
+		isPlayedMouseClick = false		
+		-- TODO
+		end
+		
+		isBruteNameActive = true
+	else 
+		isBruteNameActive = false
+	end
+	
+	-- SPEEDSTER OPTION UPDATES
+	if mouseX >= speedsterNameX and mouseX <= (speedsterNameX + speedsterNameWidth) and mouseY >= speedsterNameY and mouseY <= (speedsterNameY + speedsterNameHeight) then 
+		if not isPlayedMouseClick then
+			mouseClickMusic:play()
+			isPlayedMouseClick = true
+		end
+		
+		if mouseClicked then 
+		isPlayedMouseClick = false
+		-- TODO
+		end
+		
+		isSpeedsterNameActive = true
+	else 
+		isSpeedsterNameActive = false
+	end
+	
+	-- ACOLYTE OPTION UPDATES
+	if mouseX >= acolyteNameX and mouseX <= (acolyteNameX + acolyteNameWidth) and mouseY >= acolyteNameY and mouseY <= (acolyteNameY + acolyteNameHeight) then 
+		if not isPlayedMouseClick then
+			mouseClickMusic:play()
+			isPlayedMouseClick = true
+		end
+		
+		if mouseClicked then 
+		isPlayedMouseClick = false
+		-- TODO
+		end
+		
+		isAcolyteNameActive = true
+	else 
+		isAcolyteNameActive = false
+	end
+	
+	-- ZEALOT OPTION UPDATES
+	if mouseX >= zealotNameX and mouseX <= (zealotNameX + zealotNameWidth) and mouseY >= zealotNameY and mouseY <= (zealotNameY + zealotNameHeight) then 
+		if not isPlayedMouseClick then
+			mouseClickMusic:play()
+			isPlayedMouseClick = true
+		end
+		
+		if mouseClicked then 
+		isPlayedMouseClick = false
+		
+		-- TODO
+		end
+		
+		isZealotNameActive = true
+	else 
+		isZealotNameActive = false
+	end
+	
+	if not isReturnOptionActive and not isBruteNameActive and not isSpeedsterNameActive and not isAcolyteNameActive and not isZealotNameActive then isPlayedMouseClick = false end
 end
 
 ---------------------------------------
@@ -150,41 +345,134 @@ end
 ---------------------------------------
 function drawSelectScreen()
 				
-	love.graphics.setColor(selectScreenOptionsRGB)
+	-- GENERAL DRAW SETTINGS
+	love.graphics.setColor(OptionsRGB)
 	love.graphics.setBackgroundColor(selectScreenRGB)
 	
+	-- DRAW SELECT SCREEN TITLE
 	love.graphics.setFont(selectScreenTitleFont)
 	love.graphics.printf(gameTitle,0,gameTitleY,gameWidth,"center")
-	
-	love.graphics.setFont(selectScreenOptionFont)
+		
+	-- DRAW FIGHT OPTION
+	love.graphics.setFont(fightOptionFont)
 	love.graphics.printf(fightOption,0,fightOptionY,gameWidth,"center")
+
+	-- DRAW CLASS OPTION
+	love.graphics.setFont(classOptionFont)
 	love.graphics.printf(classOption,0,classOptionY,gameWidth,"center")
 	
-	if isFightOptionActive then		
-		love.graphics.setColor(selectScreenOptionsInFocusRGB)
+	-- OPTION HIGHLIGHTING
+	if isFightOptionActive then	
+		love.graphics.setFont(fightOptionFont)	
+		love.graphics.setColor(OptionsInFocusRGB)
 		love.graphics.printf(fightOption,0,fightOptionY,gameWidth,"center")
-		
-		if mouseClicked then drawFightScreen() end
 	else 
-		love.graphics.setColor(selectScreenOptionsRGB) 
+		love.graphics.setColor(OptionsRGB) 
 	end
 	
 	if isClassOptionActive then					
-		love.graphics.setColor(selectScreenOptionsInFocusRGB)
+		love.graphics.setFont(classOptionFont)
+		love.graphics.setColor(OptionsInFocusRGB)
 		love.graphics.printf(classOption,0,classOptionY,gameWidth,"center")
-		
-		if mouseClicked then drawClassScreen() end
 	else 
-		love.graphics.setColor(selectScreenOptionsRGB)
+		love.graphics.setColor(OptionsRGB)
 	end	
 end
 
 function drawFightScreen()
 	
+	-- GENERAL DRAW SETTINGS
+	love.graphics.setColor(OptionsRGB)
 	love.graphics.setBackgroundColor(fightScreenRGB)
+	
+	-- DRAW RETURN OPTION
+	love.graphics.setFont(returnOptionFont)
+	love.graphics.printf(returnOption,0,returnOptionY,gameWidth,"center")
+	
+	-- OPTION HIGHLIGHTING
+	if isReturnOptionActive then		
+		love.graphics.setFont(returnOptionFont)
+		love.graphics.setColor(OptionsInFocusRGB)
+		love.graphics.printf(returnOption,0,returnOptionY,gameWidth,"center")
+	else 
+		love.graphics.setColor(OptionsRGB) 
+	end
 end
 
 function drawClassScreen()
 	
+	-- GENERAL DRAW SETTINGS
+	love.graphics.setColor(OptionsRGB)
 	love.graphics.setBackgroundColor(classScreenRGB)
+	
+	-- DRAW CLASS SCREEN TITLE
+	love.graphics.setFont(classScreenTitleFont)
+	love.graphics.printf(classScreenTitle,0,classScreenTitleY,gameWidth,"center")
+	
+	-- DRAW BRUTE OPTION
+	love.graphics.setFont(bruteNameFont)
+	love.graphics.print(bruteName,bruteNameX,bruteNameY)
+	
+	-- DRAW SPEEDSTER OPTION
+	love.graphics.setFont(speedsterNameFont)
+	love.graphics.print(speedsterName,speedsterNameX,speedsterNameY)
+	
+	-- DRAW ACOLYTE OPTION
+	love.graphics.setFont(acolyteNameFont)
+	love.graphics.print(acolyteName,acolyteNameX,acolyteNameY)
+	
+	-- DRAW ZEALOT OPTION
+	love.graphics.setFont(zealotNameFont)
+	love.graphics.print(zealotName,zealotNameX,zealotNameY)
+	
+	-- DRAW RETURN OPTION
+	love.graphics.setFont(returnOptionFont)
+	love.graphics.printf(returnOption,0,returnOptionY,gameWidth,"center")
+	
+	-- DRAW CHARACTER DESCRIPTIONS
+	if isBruteNameActive then love.graphics.draw(brute, classDescriptionX, classDescriptionY) end
+	if isSpeedsterNameActive then love.graphics.draw(speedster, classDescriptionX, classDescriptionY) end
+	if isAcolyteNameActive then love.graphics.draw(acolyte, classDescriptionX, classDescriptionY) end
+	if isZealotNameActive then love.graphics.draw(zealot, classDescriptionX, classDescriptionY) end
+
+	-- OPTION HIGHLIGHTING
+	if isReturnOptionActive then	
+		love.graphics.setFont(returnOptionFont)	
+		love.graphics.setColor(OptionsInFocusRGB)
+		love.graphics.printf(returnOption,0,returnOptionY,gameWidth,"center")
+	else 
+		love.graphics.setColor(OptionsRGB) 
+	end
+	
+	if isBruteNameActive then		
+		love.graphics.setFont(bruteNameFont)
+		love.graphics.setColor(OptionsInFocusRGB)
+		love.graphics.print(bruteName,bruteNameX,bruteNameY)
+	else 
+		love.graphics.setColor(OptionsRGB) 
+	end
+	
+	if isSpeedsterNameActive then		
+		love.graphics.setFont(speedsterNameFont)
+		love.graphics.setColor(OptionsInFocusRGB)
+		love.graphics.print(speedsterName,speedsterNameX,speedsterNameY)
+	else 
+		love.graphics.setColor(OptionsRGB) 
+	end
+	
+	if isAcolyteNameActive then		
+		love.graphics.setFont(acolyteNameFont)
+		love.graphics.setColor(OptionsInFocusRGB)
+		love.graphics.print(acolyteName,acolyteNameX,acolyteNameY)
+	else 
+		love.graphics.setColor(OptionsRGB) 
+	end
+	
+	if isZealotNameActive then		
+		love.graphics.setFont(zealotNameFont)
+		love.graphics.setColor(OptionsInFocusRGB)
+		love.graphics.print(zealotName,zealotNameX,zealotNameY)
+	else 
+		love.graphics.setColor(OptionsRGB) 
+	end
 end
